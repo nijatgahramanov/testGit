5a13320419d35ddf48a72318613fb3cc394a709d
Nijat Gahramanov <nijatgahramanov00@gmail.com>
2022-09-12 23:13:38 +0300

    Nijat first commit

--- src/com/company/Bmw.java
+++
@@ -1,4 +0,0 @@
-package com.company;
-
-public class Bmw extends Car {
-}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-
+        System.out.println("testgi");
 
     }
 }
