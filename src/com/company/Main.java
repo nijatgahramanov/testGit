1d14357525e7d5436c8f16e3313275a7743b3064
Nijat Gahramanov <nejatqahramanov7@gmail.com>
2022-09-12 23:52:17 +0300

    fourth try commit by nijat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("deneme");
-        System.out.println("nijat first commit");
+        System.out.println("nijat second commit");
     }
 }
